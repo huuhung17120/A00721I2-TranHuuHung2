20c78f63dd2d7f554146f6de615dd827a31433d7
hungne <tranhuuhunghk@gmail.com>
2021-11-08 18:52:50 +0700

    update

---
+++ Module2/15 Debug/BaiTap/IllegalTriangleException/src/com/codegym/Triangle.java
@@ -0,0 +1,38 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Triangle {
+
+    public Triangle(double side1, double side2, double side3) throws IllegalTriangleException {
+        if (side1 + side2 < side3)
+            throw new IllegalTriangleException("Tam giác không hợp lệ.");
+        else if (side1 <0||side2 <0||side3 <0)
+            throw new IllegalTriangleException("Tam giác không hợp lệ.");
+        else
+            System.out.println("Tam giác hợp lệ.");
+    }
+
+    public static void main(String args[]) {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập các cạnh của tam giác: ");
+        System.out.println("Nhập cạnh thứ nhất: ");
+        int side1 = scaner.nextInt();
+        System.out.println("Nhập cạnh thứ hai: ");
+        int side2 = scaner.nextInt();
+        System.out.println("Nhập cạnh thứ ba: ");
+        int side3 = scaner.nextInt();
+        try {
+            Triangle obj = new Triangle(side1, side2, side3);
+        } catch (Exception m) {
+            System.out.println("Exception occured: " + m);
+        }
+    }
+}
+
+class IllegalTriangleException extends Exception {
+
+    IllegalTriangleException(String s) {
+        super(s);
+    }
+}
